78ca84368f619c17b78a15f94590f993f4647310
Tyler Long <tyler4long@gmail.com>
2019-07-05 16:50:56 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import RingCentral from 'ringcentral-js-concise'
+
+const rc = new RingCentral(
+  process.env.RINGCENTRAL_CLIENT_ID,
+  process.env.RINGCENTRAL_CLIENT_SECRET,
+  process.env.RINGCENTRAL_SERVER_URL
+)
+
+;(async () => {
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD
+  })
+  console.log(rc.token())
+  await rc.revoke()
+})()
