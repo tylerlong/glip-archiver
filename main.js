c2e341548d8d8f6c9a1f7deb32e1c54c9777dc53
Tyler Long <tyler4long@gmail.com>
2019-07-09 17:57:39 -0700

    Auto save to cookie

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { Component } from 'react-subx'
 import SubX from 'subx'
 import Cookies from 'js-cookie'
 import RingCentral from 'ringcentral-js-concise'
+import delay from 'timeout-as-promise'
 
 const redirectUri = 'http://localhost:8080'
 
@@ -14,13 +15,19 @@ const rc = new RingCentral(
 )
 
 const store = SubX.create(Cookies.getJSON('glip-archiver'))
+SubX.autoRun(store, () => {
+  Cookies.set('glip-archiver', store.toJSON(), { expires: 3650 })
+})
 
+// 3-legged oauth
 const urlParams = new URLSearchParams(window.location.search)
 const code = urlParams.get('code')
 if (code) {
   (async () => {
     await rc.authorize({ code, redirectUri })
     store.token = rc.token()
+    await delay(100)
+    window.location.href = redirectUri
   })()
 }
 
@@ -31,7 +38,10 @@ class Hello extends Component {
     if (!store.token) {
       body = <a href={rc.authorizeUri(redirectUri)}>Authorize me to access your Glip data</a>
     } else {
-      body = <h2>Has token: {store.token.access_token}</h2>
+      body = <>
+        <h2>Archive your Glip data</h2>
+
+      </>
     }
     return <>
       <h1>Glip Archiver</h1>
