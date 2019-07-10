4be2d760564d1901b425a921b8a6250ec654d8df
Tyler Long <tyler4long@gmail.com>
2019-07-09 17:39:03 -0700

    Get token

--- main.js
+++ main.js
@@ -3,24 +3,35 @@ import ReactDOM from 'react-dom'
 import { Component } from 'react-subx'
 import SubX from 'subx'
 import Cookies from 'js-cookie'
-// import RingCentral from 'ringcentral-js-concise'
+import RingCentral from 'ringcentral-js-concise'
 
-// const rc = new RingCentral()
+const redirectUri = 'http://localhost:8080'
 
-console.log(process.env)
+const rc = new RingCentral(
+  process.env.RINGCENTRAL_CLIENT_ID,
+  process.env.RINGCENTRAL_CLIENT_SECRET,
+  process.env.RINGCENTRAL_SERVER_URL
+)
 
-const store = SubX.create({
-  cookies: Cookies.getJSON('glip-archiver')
-})
+const store = SubX.create(Cookies.getJSON('glip-archiver'))
+
+const urlParams = new URLSearchParams(window.location.search)
+const code = urlParams.get('code')
+if (code) {
+  (async () => {
+    await rc.authorize({ code, redirectUri })
+    store.token = rc.token()
+  })()
+}
 
 class Hello extends Component {
   render () {
-    const cookies = this.props.cookies || {}
+    const store = this.props.store
     let body
-    if (!cookies.token) {
-      body = <h2>No token</h2>
+    if (!store.token) {
+      body = <a href={rc.authorizeUri(redirectUri)}>Authorize me to access your Glip data</a>
     } else {
-      body = <h2>Has token</h2>
+      body = <h2>Has token: {store.token.access_token}</h2>
     }
     return <>
       <h1>Glip Archiver</h1>
@@ -29,4 +40,4 @@ class Hello extends Component {
   }
 }
 
-ReactDOM.render(<Hello cookies={store.cookies} />, document.getElementById('container'))
+ReactDOM.render(<Hello store={store} />, document.getElementById('container'))
